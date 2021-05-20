ba72c0221d532a2ecad9f655ff844b74c1bb1d12
Decho Kocharin <sourcedecho@outlook.com>
2021-05-20 12:38:41 +0700

    Update encoding.c

--- stratum/algos/ar2/encoding.c
+++ stratum/algos/ar2/encoding.c
@@ -4,12 +4,12 @@
  * Copyright 2015
  * Daniel Dinu, Dmitry Khovratovich, Jean-Philippe Aumasson, and Samuel Neves
  *
- * You may use this work under the terms of a Creative Commons CC0 1.0 
+ * You may use this work under the terms of a Creative Commons CC0 1.0
  * License/Waiver or the Apache Public License 2.0, at your option. The terms of
  * these licenses can be found at:
  *
- * - CC0 1.0 Universal : http://creativecommons.org/publicdomain/zero/1.0
- * - Apache 2.0        : http://www.apache.org/licenses/LICENSE-2.0
+ * - CC0 1.0 Universal : https://creativecommons.org/publicdomain/zero/1.0
+ * - Apache 2.0        : https://www.apache.org/licenses/LICENSE-2.0
  *
  * You should have received a copy of both of these licenses along with this
  * software. If not, they may be obtained at the above URLs.
@@ -325,11 +325,11 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
 
     CC("$");
     CC(type_string);
-	
+
     /* Reading the version number if the default is suppressed */
     ctx->version = ARGON2_VERSION_10;
     CC_opt("$v=", DECIMAL_U32(ctx->version));
-	
+
     CC("$m=");
     DECIMAL_U32(ctx->m_cost);
     CC(",t=");
@@ -414,6 +414,9 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
 
     SS("$");
     SS(type_string);
+
+    SS("$v=");
+    SX(ctx->version);
 
     SS("$m=");
     SX(ctx->m_cost);
