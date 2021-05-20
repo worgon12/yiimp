153b8849c9118cdacf39773e8c5df18e7f1a44e5
Decho Kocharin <sourcedecho@outlook.com>
2021-05-20 23:40:48 +0700

    Update stratum.h

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -221,7 +221,6 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/tribus.h"
 #include "algos/veltor.h"
 #include "algos/velvet.h"
-#include "algos/argon2a.h"
 #include "algos/vitalium.h"
 #include "algos/aergo.h"
 #include "algos/hex.h"
