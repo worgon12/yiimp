950288c6e70b657f9820ab612fce6d91c7c35271
Decho Kocharin <sourcedecho@outlook.com>
2021-06-27 19:43:35 +0700

    Revert "Update coinbase.cpp"
    This reverts commit 21b63b91d85922036fb7b20e9c636f28da56bc52.

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -975,16 +975,9 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 	strcat(templ->coinb2, script_dests);
 	job_pack_tx(coind, templ->coinb2, available, NULL);
 	strcat(templ->coinb2, "00000000"); // locktime
-	if(coinbase_payload && strlen(coinbase_payload) > 0)
-	{
-		char coinbase_payload_size[18];
-		ser_compactsize((unsigned int)(strlen(coinbase_payload) >> 1), coinbase_payload_size);
-		strcat(templ->coinb2, coinbase_payload_size);
-		strcat(templ->coinb2, coinbase_payload);
-	}
 	coind->reward = (double)available / 100000000 * coind->reward_mul;
 	return;
-}
+	}
 
 	//  add BMN
 	if (strcmp(coind->symbol, "BMN") == 0)
