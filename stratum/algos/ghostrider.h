ae4588273a4e1a55627789c39928b305b85fde2a
Worgon12 <3902391+worgon12@users.noreply.github.com>
2022-04-18 21:39:33 +0200

    rename gr to ghostrider

--- stratum/algos/ghostrider.h
+++ stratum/algos/ghostrider.h
@@ -6,7 +6,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void gr_hash(const char* input, char* output, uint32_t len);
+void ghostrider_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
